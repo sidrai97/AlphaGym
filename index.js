b4a0fcee7b8b34832757bac4dae9b51a2199545b
Siddhant Rai <siddhantraisgr@gmail.com>
2017-09-01 20:01:08 +0530

    added echo functionality

--- index.js
+++ index.js
@@ -17,15 +17,47 @@ app.get('/', function(req, res) {
 	res.send("Hi, I am a AlphaBot")
 })
 
-let token = "sidrai97"
+let token = "EAAHTevQllzYBAPueASJIDAWlCJnWiZCYy0M9KmVv3Tjw2yXTUkdROnIf3ZCqzu6uSeTKDFIgGNMioocOJ08m0qY516b9C2FF7bA8l3XsyMto27a4wn3cqzMxrOhyaF5wuKtk5ZCyB6iZAXbf8wcCqsKgZCLKGUFS5zbYW245O8wZDZD"
 
 // Facebook 
 app.get('/webhook/', function(req, res) {
-	if (req.query['hub.verify_token'] === token) {
+	if (req.query['hub.verify_token'] === "sidrai97") {
 		res.send(req.query['hub.challenge'])
 	}
 	res.send("Wrong token")
 })
+
+app.post('/webhook/', function(req, res) {
+	let messaging_events = req.body.entry[0].messaging
+	for (let i = 0; i < messaging_events.length; i++) {
+		let event = messaging_events[i]
+		let sender = event.sender.id
+		if (event.message && event.message.text) {
+			let text = event.message.text
+			sendText(sender, "Text echo: " + text.substring(0, 100))
+		}
+	}
+	res.sendStatus(200)
+})
+
+function sendText(sender, text) {
+	let messageData = {text: text}
+	request({
+		url: "https://graph.facebook.com/v2.6/me/messages",
+		qs : {access_token: token},
+		method: "POST",
+		json: {
+			recipient: {id: sender},
+			message : messageData,
+		}
+	}, function(error, response, body) {
+		if (error) {
+			console.log("sending error")
+		} else if (response.body.error) {
+			console.log("response body error")
+		}
+	})
+}
 
 // run app
 app.listen(app.get('port'), function() {
