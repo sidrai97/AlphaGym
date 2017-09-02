f026e20e167585b1ed2f1c1fe438ad1c99126697
Siddhant Rai <siddhantraisgr@gmail.com>
2017-09-02 20:18:06 +0530

    changes in echo bot

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const bodyParser = require('body-parser')
 const request = require('request')
 
 const app = express()
+const token = process.env.FB_VERIFY_TOKEN
+const access = process.env.FB_ACCESS_TOKEN
 
 app.set('port', (process.env.PORT || 5000))
 
@@ -17,46 +19,107 @@ app.get('/', function(req, res) {
 	res.send("Hi, I am a AlphaBot")
 })
 
-let token = "EAAHTevQllzYBAPueASJIDAWlCJnWiZCYy0M9KmVv3Tjw2yXTUkdROnIf3ZCqzu6uSeTKDFIgGNMioocOJ08m0qY516b9C2FF7bA8l3XsyMto27a4wn3cqzMxrOhyaF5wuKtk5ZCyB6iZAXbf8wcCqsKgZCLKGUFS5zbYW245O8wZDZD"
-
 // Facebook 
-app.get('/webhook/', function(req, res) {
-	if (req.query['hub.verify_token'] === "sidrai97") {
-		res.send(req.query['hub.challenge'])
-	}
-	res.send("Wrong token")
-})
+app.get('/webhook', function(req, res) {
+	if (req.query['hub.mode'] === 'subscribe' && req.query['hub.verify_token'] === token){
+		console.log("Validating webhook");
+		res.status(200).send(req.query['hub.challenge']);
+	} 
+	else{
+	  console.error("Failed validation. Make sure the validation tokens match.");
+	  res.sendStatus(403);          
+	}  
+});
 
-app.post('/webhook/', function(req, res) {
-	let messaging_events = req.body.entry[0].messaging
-	for (let i = 0; i < messaging_events.length; i++) {
-		let event = messaging_events[i]
-		let sender = event.sender.id
-		if (event.message && event.message.text) {
-			let text = event.message.text
-			sendText(sender, "Text echo: " + text.substring(0, 100))
-		}
-	}
-	res.sendStatus(200)
-})
+app.post('/webhook', function (req, res) {
+	var data = req.body;  
 
-function sendText(sender, text) {
-	let messageData = {text: text}
+	// Make sure this is a page subscription
+	if (data.object === 'page') {
+		// Iterate over each entry - there may be multiple if batched
+		data.entry.forEach(function(entry) {
+			var pageID = entry.id;
+			var timeOfEvent = entry.time;
+  
+			// Iterate over each messaging event
+			entry.messaging.forEach(function(event) {
+			if (event.message) {
+				receivedMessage(event);
+			} 
+			else {
+				console.log("Webhook received unknown event: ", event);
+			}
+			});
+		});
+		res.sendStatus(200);
+	}
+});
+	
+function receivedMessage(event) {
+	var senderID = event.sender.id;
+	var recipientID = event.recipient.id;
+	var timeOfMessage = event.timestamp;
+	var message = event.message;
+
+	console.log("Received message for user %d and page %d at %d with message:", senderID, recipientID, timeOfMessage);
+	console.log(JSON.stringify(message));
+  
+	var messageId = message.mid;
+	var messageText = message.text;
+	var messageAttachments = message.attachments;
+  
+	if (messageText) {
+	  // If we receive a text message, check to see if it matches a keyword
+	  // and send back the example. Otherwise, just echo the text we received.
+		switch (messageText) {
+			case 'generic':
+		  	sendGenericMessage(senderID);
+		  	break;
+  
+			default:
+		  	sendTextMessage(senderID, messageText);
+	  	}
+	} 
+	else if (messageAttachments) {
+		sendTextMessage(senderID, "Message with attachment received");
+	}
+}
+
+function sendGenericMessage(recipientId, messageText) {
+	// To be expanded in later sections
+}
+
+function sendTextMessage(recipientId, messageText) {
+	var messageData = {
+		recipient: {
+			id: recipientId
+	  	},
+	  	message: {
+			text: messageText
+	  	}
+	};
+	callSendAPI(messageData);
+}
+
+function callSendAPI(messageData) {
 	request({
-		url: "https://graph.facebook.com/v2.6/me/messages",
-		qs : {access_token: token},
-		method: "POST",
-		json: {
-			recipient: {id: sender},
-			message : messageData,
+		uri: 'https://graph.facebook.com/v2.6/me/messages',
+		qs: { access_token: access },
+		method: 'POST',
+		json: messageData
+	}, function (error, response, body) {
+		if (!error && response.statusCode == 200) {
+			var recipientId = body.recipient_id;
+			var messageId = body.message_id;
+
+			console.log("Successfully sent generic message with id %s to recipient %s", messageId, recipientId);
+		} 
+		else {
+			console.error("Unable to send message.");
+			console.error(response);
+			console.error(error);
 		}
-	}, function(error, response, body) {
-		if (error) {
-			console.log("sending error")
-		} else if (response.body.error) {
-			console.log("response body error")
-		}
-	})
+	});  
 }
 
 // run app
