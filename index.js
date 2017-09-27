db2d01d5cf91b27412f77a109c53c5bbc50727c3
Urvi Paithankar <akshay.raut0303@gmail.com>
2017-09-27 18:47:50 +0530

    ajax moved to server-trial

--- index.js
+++ index.js
@@ -225,8 +225,8 @@ app.get('/stats', function(req, resp) {
 			};
 
 			http.request(options, function(res) {
-			//console.log('STATUS: ' + res.statusCode);
-			//console.log('HEADERS: ' + JSON.stringify(res.headers));
+			console.log('STATUS: ' + res.statusCode);
+			console.log('HEADERS: ' + JSON.stringify(res.headers));
 			//res.setEncoding('utf8');
 			res.on('data', function (chunk) {
 				console.log('BODY: ' + chunk);
